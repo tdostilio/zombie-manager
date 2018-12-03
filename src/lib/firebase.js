b0c35dd3e1b868bc87f167af5c0feaea3ad51d4d
Tom Dostilio <tdostilio@gmail.com>
2018-12-03 11:43:16 -0500

    updates code to work with react_app env variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,9 +1,20 @@
+import dotenv from "dotenv"
 import * as firebase from "firebase"
 import "firebase/firestore"
-require("dotenv").config()
+dotenv.config()
 
+console.log(process.env)
 //put firebase config information here
-const config = process.env.FB_CONFIG
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJ_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID
+}
+
+console.log(config)
 
 const fire = firebase.initializeApp(config)
 
